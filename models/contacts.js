5015a9441c48a0e7bdbb4ee8e546dd81593be8ae
Vishwas <90545804+LuffyXD9@users.noreply.github.com>
2023-11-20 20:22:15 +0530

    added validations on api's

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 const contactSchema = new mongoose.Schema({
-    // id : {type : Date, immutable : true, default : Date.now()},
     firstName : String,
     lastName : String,
     nickName : String,
-    DOB : Date,
+    DOB : {
+        type : Date,
+    },
     mobileNumbers : [Number],
     emails : {type : [String], lowercase : true}
 })
-
 
 module.exports = mongoose.model("contacts",contactSchema);
--- routes/allContacts.js
+++ routes/allContacts.js
@@ -16,12 +16,12 @@ router.get('/', async(req,res)=>{
 //get one
 router.get('/:id', getContact, (req,res)=>{
     // console.log(res.contact);
-    res.send(res.contact);
+    res.send(req.contact);
 })
 
 //create one
-router.post('/', async(req,res)=>{
-    // console.log(req.body.id);
+router.post('/', validateContact ,async(req,res)=>{
+    //add validations
     const contact = new Contacts({
         firstName : req.body.firstName,
         lastName : req.body.lastName,
@@ -30,6 +30,8 @@ router.post('/', async(req,res)=>{
         mobileNumbers : req.body.mobileNumbers,
         emails : req.body.emails,
     })
+
+    // const contact = new Contacts(req.body);
     try {
         const newContact = await contact.save();
         res.status(200).json(newContact);
@@ -38,13 +40,14 @@ router.post('/', async(req,res)=>{
     }
 })
 //update one
-router.patch('/:id', getContact, async(req,res)=>{
+router.patch('/:id', getContact, validateContact, async(req,res)=>{
     if(req.body){
-        Object.assign(res.contact, req.body);
+        Object.assign(req.contact, req.body);
+        //add validations on backend while updating
     }
     try{
         // console.log(res.contact);
-        const updatedContact = await res.contact.save();
+        const updatedContact = await req.contact.save();
         res.json(updatedContact);
     }catch(err){
         res.status(400).json({message : err.message});
@@ -54,26 +57,48 @@ router.patch('/:id', getContact, async(req,res)=>{
 //delete one
 router.delete('/:id', getContact, async(req,res)=>{
     try {
-        await res.contact.deleteOne();
+        await req.contact.deleteOne();
         res.status(200).json({message : "Contact Deleted"});
     } catch (err) {
         res.status(500).json({message  : err.message});
     }
-
 })
 
 async function getContact(req, res, next){
-    let contact;
     try {
-        contact = await Contacts.findById(req.params.id);
-        if(!contact){
-            return res.status(404).json({message : "No contact"})
+        if (!req.params.id) {
+            return res.status(400).json({ message: "Missing contact id in the request" });
         }
-        res.contact = contact;
+        const contact = await Contacts.findById(req.params.id);
+
+        if (!contact) {
+            return res.status(404).json({ message: "No contact" });
+        }
+        req.contact = contact;
         next();
     } catch (err) {
-        return res.status(500).json({message : err.message})
+        return res.status(500).json({ message: err.message });
     }
-    
 }
+
+function validateContact(req, res, next) {
+    const { firstName, lastName, nickName, mobileNumbers, emails } = req.body;
+
+    if (!firstName || !lastName || !nickName || !mobileNumbers || !emails) {
+        return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    if (!Array.isArray(mobileNumbers) || mobileNumbers.some(number => !(/^\d{10}$/.test(number)))) {
+        return res.status(400).json({ message: "Invalid mobile number format" });
+    }
+
+    if (!Array.isArray(emails) || emails.some(email => !(/\S+@\S+\.\S+/.test(email)))) {
+        return res.status(400).json({ message: "Invalid email address format" });
+    }
+
+    next();
+}
+
 module.exports = router
+
+//use req instead of res in getContact
